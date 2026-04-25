9c57558a0388508650d12acb29c8ef53f3c1c68a
VEDANT GHARAT <vedantgharat37@gmail.com>
2026-04-25 22:49:12 +0530

    Remove API key and measurement ID from config
    Removed sensitive API key and measurement ID from firebase config.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9592410207-7fea6",
   "appId": "1:1095320005353:web:0e3b2918f136d5e4ee1e78",
-  "apiKey": "AIzaSyCDw-u3dYqcwStuup8spA1OSx55xpiy9pw",
+  "apiKey": "",
   "authDomain": "studio-9592410207-7fea6.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "1095320005353"
